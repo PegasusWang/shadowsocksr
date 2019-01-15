83d8327ba1a17e812c8b9791c4f0f89d52dcc0bb
wangningning <wangningning@zhihu.com>
2019-01-15 12:22:42 +0800

    add parse

--- rixcloud_ssr_to_gui-config.py
+++ rixcloud_ssr_to_gui-config.py
@@ -27,6 +27,41 @@ def _fill_missing(string):
 def _b64_url_decode(b64_str):
     return base64.urlsafe_b64decode(_fill_missing(b64_str)).decode()
 
+def parsed_ssr_url(url):
+    """
+    url: "ssr://"
+    """
+    ssr_url = url[6:]  # remove prefix 'ssr://'
+    parsed_url = base64.urlsafe_b64decode(_fill_missing(ssr_url)).decode()
+    url_info, params_str = parsed_url.split('/?')
+    config_dict = {}
+    server, server_port, protocol, method, obfs, password_b64 = url_info.split(':')
+    config_dict = {
+        "server": server,
+        "serve_port": server_port,
+        "protocol": protocol,
+        "method": method,
+        "obfs": obfs,
+        "password": _b64_url_decode(password_b64),
+        "enable": True,
+    }
+    obfs_params = {}
+    for pair in params_str.split('&'):
+        k, v = pair.split('=')
+        obfs_params[k] = _b64_url_decode(v)
+    config_dict.update(obfs_params)
+    return config_dict
+
+
+def parse_text_ssr(text):
+    """rixcloud 端口复用配置"""
+    urls = [i for i in text.split() if i]
+    configs = []
+    for url in urls:
+        config_dict = parsed_ssr_url(url)
+        configs.append(config_dict)
+    return configs
+
 
 def parse():
     url = os.getenv(ENV_KEY)
@@ -37,37 +72,28 @@ def parse():
     urls = [_ for _ in urls if _]
     configs = []
     for url in urls:
-        ssr_url = url[6:]  # remove prefix 'ssr://'
-        parsed_url = base64.urlsafe_b64decode(_fill_missing(ssr_url)).decode()
-        url_info, params_str = parsed_url.split('/?')
-        config_dict = {}
-        server, server_port, protocol, method, obfs, password_b64 = url_info.split(':')
-        config_dict = {
-            "server": server,
-            "serve_port": server_port,
-            "protocol": protocol,
-            "method": method,
-            "obfs": obfs,
-            "password": _b64_url_decode(password_b64),
-            "enable": True,
-        }
-        obfs_params = {}
-        for pair in params_str.split('&'):
-            k, v = pair.split('=')
-            obfs_params[k] = _b64_url_decode(v)
-        config_dict.update(obfs_params)
-
+        config_dict = parsed_ssr_url(url)
         configs.append(config_dict)
     return configs
 
 
-def main():
+def write_gui_config():
     res = {}
     configs = parse()
     res['configs'] = configs
     with open('gui-config.json', 'w') as f:
         json.dump(res, f, indent=4, ensure_ascii=False)
 
+def write_gui_config_ssr_common():
+    """端口复用配置写入"""
+    with open('_ssr.txt') as f:
+        text = f.read().strip()
+    res = {}
+    configs = parse_text_ssr(text)
+    res['configs'] = configs
+    with open('gui-config.json', 'w') as f:
+        json.dump(res, f, indent=4, ensure_ascii=False)
 
 if __name__ == '__main__':
-    main()
+    # write_gui_config_ssr_common()
+    write_gui_config()
